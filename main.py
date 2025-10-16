293dc7ea273ddc071b6da85f9f6f4a8d0cb7a47d
yeabsraesubalew1-sketch <yeabsraesubalew1@gmail.com>
2025-10-16 11:54:12 +0300

    yeabsra

--- main.py
+++ main.py
@@ -12,22 +12,61 @@ while True:
     # -------------------------------
     # Group 1: Check Balance
     if option == "1":
-        pass   # <-- Group 1: Insert your code here
+        input(f"Your current balance is {balance} Birr. ")
 
     # -------------------------------
     # Group 2: Buy Data
     elif option == "2":
         
-        pass   # <-- Group 2: Insert your code here
+        packages = [
+            {"mb": 50, "price": 10},
+            {"mb": 100, "price": 20},
+            {"mb": 150, "price": 30},
+            {"mb": 200, "price": 40}
+        ]
+        print("--Buy Data--")
+        for i, pack in enumerate(packages, start=1):
+            print(f"{i}. {pack["mb"]}MB for {pack["price"]}Birr")
+        print("0. Go back")
+        data = input("Which do you want? ")
+        try:
+            data_num = int(data)
+        except Exception as e:
+            input(f"Error: {e} ")
+        else:
+            if data_num == 0:
+                pass
+            elif data_num - 1 in range(len(packages)):
+                if packages[data_num - 1]["price"] <= balance:
+                    balance -= packages[data_num - 1]["price"]
+                    print(f"You have successfully purchased {packages[data_num - 1]["mb"]}MB for {packages[data_num - 1]["price"]}Birr")
+                    input(f"Your current balance is {balance} Birr. ")
+                else:
+                    input("Insufficient balance! ")
+            else:
+                input("Invalid input! ")
 
     # -------------------------------
     # Group 3: Buy Airtime
     elif option == "3":
-        pass   # <-- Group 3: Insert your code here
+        print("--Buy Airtime--")
+        airtime = input("How much Airtime would you like to purchase? ")
+        try:
+            airtime_num = int(airtime)
+        except Exception as e:
+            input(f"Error: {e}")
+        else:
+            if airtime_num <= balance:
+                balance -= airtime_num
+                print(f"You have successfully purchased {airtime_num}Birr in Airtime")
+                input(f"Your current balance is {balance} Birr. ")
+            else:
+                input("Insufficient balance!! ")
+
 
     # -------------------------------
     # Group 4: Exit & Invalid Input
     elif option == "4":
-        pass   # <-- Group 4: Insert your code to handle exit here
+        exit(0)
     else:
-        pass   # <-- Group 4: Insert your code to handle invalid input here
+        input("Invalid input!! ")
