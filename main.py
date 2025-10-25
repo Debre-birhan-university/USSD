cb9e8f7d78b8196ee3ed463435d1ee93fd9a33d9
Abebe Asnakew <abebeasnakew38@gmail.com>
2025-10-26 03:18:08 +0900

    simple USSD code

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
 balance = 100  # Starting balance
-
 while True:
     print("\nUSSD Menu:")
     print("1. Check Balance")
     print("2. Buy Data")
     print("3. Buy Airtime")
-    print("4. Exit")
-    
+    print("4. Deposite Balance ")
+    print("5. Exit")
     option = input("Enter option: ")
 
-    # -------------------------------
-    # Group 1: Check Balance
     if option == "1":
-        pass   # <-- Group 1: Insert your code here
-
-    # -------------------------------
-    # Group 2: Buy Data
+        print (f"Your available balance is:" ,balance,"ETB")
+        break
     elif option == "2":
-        
-        pass   # <-- Group 2: Insert your code here
-
-    # -------------------------------
-    # Group 3: Buy Airtime
+        phone=input("Enter recharged mobile number: ") #use 0912345678 to be identical with our phone
+        if phone =="0912345678":
+            amount=int(input("Enter your amount: "))
+            if amount<=balance:
+                balance=balance-amount
+                print("Successful,You buy", amount,"ETB Data pakege and Your current balance is ",balance,"ETB")
+            else:
+                print ("Your balance is insufficient.")
+        else:
+            print ("Incorrect phone number,Please try again!")
+        break
     elif option == "3":
-        pass   # <-- Group 3: Insert your code here
-
-    # -------------------------------
-    # Group 4: Exit & Invalid Input
+        phone=input("Enter recharged mobile number: ") #use 0912345678 to be identical with our phone
+        if phone =="0912345678":
+            amount=int(input("Enter your amount: "))
+            if amount<=balance:
+                balance=balance-amount
+                print("Successful,You buy", amount,"ETB air time and Your current balance is ",balance,"ETB")
+            else:
+                print ("Your balance is insufficient.")
+        else:
+            print ("Incorrect phone number,Please try again!")
+        break
     elif option == "4":
-        pass   # <-- Group 4: Insert your code to handle exit here
+       deposite=int(input("Enter Deposit amount: "))
+       balance=balance+deposite
+       print("Successful,You deposite", deposite,"ETB and Your current balance is ",balance,"ETB")
+       break
+    elif option == "5":
+        print ("Good bay.")
+        break
     else:
-        pass   # <-- Group 4: Insert your code to handle invalid input here
+       print("Please insert correct option.")
